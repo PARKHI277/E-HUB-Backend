93104facf7198f75eaf081723336f877e9e4e066
Sukriti <sukritimaurya.fzd@gmail.com>
2022-09-10 10:05:46 +0530

    new

--- app.js
+++ app.js
@@ -8,25 +8,30 @@ const SignupRouter = require("./routers/signup");
 const LoginRouter = require("./routers/login");
 const BodyParser = require("body-parser");
 const handbookrouter = require("./routers/handbook");
+const eventRouter = require("./routers/event");
 const resourcerouter = require("./routers/resource");
 const mentorrouter = require("./routers/mentor");
 const hiringrouter = require("./routers/hiring");
 const industryrouter = require("./routers/industrypersonality");
 const internshipRouter = require("./routers/internship");
 const youtube = require("./routers/youtubeid");
-
+const campusactivity = require("./routers/campus");
 const collegerouter = require("./routers/college");
 const teamrouter = require("./routers/team");
+const testimonialRouter=require("./routers/testimonial");
 const branchrouter = require("./routers/Branch");
 const passport = require("passport");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const errorController = require('./controllers/errorController');
+const domainrouter = require("./routers/domain");
+
 
 const app = express();
 require("./config/passport")(passport);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
-
+app.use(errorController);
 // SESSION MIDDLEWARE
 app.use(
   session({
@@ -67,7 +72,7 @@ app.use(express.json());
 //routers
 app.use("/api/v1", SignupRouter);
 app.use("/api/v1", LoginRouter);
-
+app.use("/api/v1", eventRouter);
 app.use("/api/v1", resourcerouter);
 app.use("/api/v1", mentorrouter);
 app.use("/api/v1", hiringrouter);
@@ -76,9 +81,11 @@ app.use("/api/v1", internshipRouter);
 app.use("/api/v1", handbookrouter);
 app.use("/api/v1", collegerouter);
 app.use("/api/v1", teamrouter);
+app.use("/api/v1", testimonialRouter);
 app.use("/api/v1", branchrouter);
 app.use("/api/v1", youtube);
-
+app.use("/api/v1", campusactivity);
+app.use("/api/v1", domainrouter);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
