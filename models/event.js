5e008e577af01e8722d0a9f0046f78cbd0ff9590
Parkhi <parkhigarg277@gmail.com>
2022-09-20 09:42:30 +0530

    event code

--- models/event.js
+++ models/event.js
@@ -1,54 +1,58 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const validator=require('validator');
+const validator = require("validator");
 const Schema = mongoose.Schema;
-const eventSchema = new Schema({
-  //eventId: {
-//   type:Number,
-//   unique:true,
-//   require:true, 
-//   default:0
-// },
-  mentorName: {
-    type: String,
-    required: [true,'Enter a mentor name.']
+const eventSchema = new Schema(
+  {
+    //eventId: {
+    //   type:Number,
+    //   unique:true,
+    //   require:true,
+    //   default:0
+    // },
+    mentorName: {
+      type: String,
+      required: [true, "Enter a mentor name."],
+    },
+    mentorImage: {
+      type: Array,
+      required: [true, "Enter a mentor image."],
+    },
+    eventName: {
+      type: String,
+      required: [true, "Enter an event name."],
+      unique: true,
+    },
+    eventCode: {
+      type: String,
+      unique: true,
+      default: null,
+    },
+    description: {
+      type: String,
+      required: [true, "Enter a description."],
+    },
+    eventDate: {
+      type: Date,
+      required: [true, "Enter date and time."],
+    },
+    position: {
+      type: String,
+    },
+    tagline: {
+      type: String,
+    },
+    company: {
+      type: String,
+    },
+    posterUrl: {
+      type: String,
+      required: [true, "Enter poster url."],
+      unique: true,
+    },
   },
-  mentorImage: {
-    type: Array,
-    required: [true,'Enter a mentor image.']
-  },
-  eventName: {
-    type: String,
-    required: [true,'Enter an event name.'],
-    unique:true
-  },
-  eventCode: {
-    type: String,
-    unique:true
-  },
-  description: {
-    type: String,
-    required: [true,'Enter a description.']
-  },
-  eventDate: {
-    type: Date,
-    required: [true,'Enter date and time.']
-  },
-  position: {
-    type: String
-  },
-  tagline: {
-    type: String
-  },
-  company: {
-    type: String,
-  },
-  posterUrl: {
-    type: String,
-    required: [true,'Enter poster url.'],
-    unique:true
-  }
-},  { timestamps: true });
+  { timestamps: true }
+);
 
 const Event = new mongoose.model("Event", eventSchema);
 module.exports = Event;
