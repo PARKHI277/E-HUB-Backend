d6abce329267be85701d86d24d4f1ae4e707adac
Parkhi <parkhigarg277@gmail.com>
2022-09-29 10:29:00 +0530

    query router

---
+++ models/query.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const validate = require("validator");
+
+const Schema = mongoose.Schema;
+
+const querySchema = new Schema(
+  {
+    query: {
+      type: Array,
+      url: { type: String },
+      domain: { type: String },
+      required: [true, "Enter Query"],
+    },
+  },
+  { timestamps: true }
+);
+
+const query = new mongoose.model("query", querySchema);
+module.exports = query;
