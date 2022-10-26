f73bdb55c7e68c7849a5d0cfda0669bce0d6afb2
Parkhi <parkhigarg277@gmail.com>
2022-10-26 13:54:05 +0530

    update

--- connection/data.js
+++ connection/data.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.gmmfk.mongodb.net/${process.env.DB}`,
-  { useNewUrlParser: true },
-  () => console.log("Successfully connected to mongodb database")
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () =>
+  console.log("Successfully connected to mongodb database")
 );
